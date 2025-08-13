d68ae984f356d558869c948da489ff1029ee557f
Alinda Byamukama <alindabyamukama@gmail.com>
2025-08-13 20:08:08 +0300

    fix bounding box

--- src/App.js
+++ src/App.js
@@ -81,15 +81,15 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const clarifaiFace = data[0].data.regionsList[0].regionInfo.boundingBox;
     const image = document.getElementById('inputImage')
     const width = Number(image.width)
     const height = Number(image.height)
     return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
+      leftCol: clarifaiFace.leftCol * width,
+      topRow: clarifaiFace.topRow * height,
+      rightCol: width - (clarifaiFace.rightCol * width),
+      bottomRow: height - (clarifaiFace.bottomRow * height)
     }
   }
 
